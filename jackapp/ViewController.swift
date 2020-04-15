07a20451cabe3f9d62949d4f5b5f9b8d3c8aac4a
jsulli75 <jsulli75@villanova.edu>
2020-04-15 18:06:04 -0400

    Initial Commit

---
+++ jackapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jackapp
+//
+//  Created by Jack Sullivan on 4/15/20.
+//  Copyright © 2020 Jack Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
