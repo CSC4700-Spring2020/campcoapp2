ce81880db8589028c6c3aaee2b6ea123af30ef86
jsulli75 <jsulli75@villanova.edu>
2020-04-15 23:00:14 -0400

    Cleaned up menu section and added room for a cart

--- jackapp/ViewController.swift
+++ jackapp/ViewController.swift
@@ -42,25 +42,49 @@ class MenuViewController: UIViewController {
 
     @IBOutlet weak var menulist: UITextView!
     
+    @IBOutlet weak var fav: UILabel!
+    @IBOutlet weak var fav1: UILabel!
+    @IBOutlet weak var fav2: UILabel!
+    @IBOutlet weak var fav4: UILabel!
+    @IBOutlet weak var fav3: UILabel!
+    @IBOutlet weak var fav5: UILabel!
+    @IBOutlet weak var fav6: UILabel!
+    @IBOutlet weak var fav7: UILabel!
+    @IBOutlet weak var fav8: UILabel!
     
+    
+    @IBOutlet weak var but1: UIButton!
+    @IBOutlet weak var but2: UIButton!
+    @IBOutlet weak var but3: UIButton!
+    @IBOutlet weak var but4: UIButton!
+    @IBOutlet weak var but5: UIButton!
+    @IBOutlet weak var but6: UIButton!
+    @IBOutlet weak var but7: UIButton!
+    @IBOutlet weak var but8: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         view.backgroundColor = .white
-    }
+        
+        let checkoutButton = UIBarButtonItem(title: "Checkout!", style: UIBarButtonItem.Style.plain, target: self, action: "checkout")    }
 
 
 }
 
 class CheckViewController: UIViewController {
 
+    @IBOutlet weak var cart: UILabel!
+    
+    
+    @IBOutlet weak var checkout: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        view.backgroundColor = .green
+        view.backgroundColor = .lightGray
     }
 
 
