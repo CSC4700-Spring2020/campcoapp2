5030d455667203bb9099eaf4a3bf05b95fd061be
jsulli75 <jsulli75@villanova.edu>
2020-04-15 21:51:53 -0400

    first sprint with homepage and starting menu

--- jackapp/ViewController.swift
+++ jackapp/ViewController.swift
@@ -10,24 +10,45 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    
+    @IBOutlet weak var campoappname: UITextField!
+    @IBOutlet weak var campco: UIImageView!
+    
+        
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        view.backgroundColor = .red
+        view.backgroundColor = .systemTeal
+        /*
+        let attributedString = NSMutableAttributedString (string: "access the webpage")
+        attributedString.addAttribute(.link, value: "https://www.campuscornerpizza.com", range: NSRange(location: 19, length:55))
+        
+        textView.attributedText = attributedString
+        */
+        
+       
     }
-
-
+    
+    
+    
+    
 }
 
 
 class MenuViewController: UIViewController {
 
+    @IBOutlet weak var menulist: UITextView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
     }
 
 
